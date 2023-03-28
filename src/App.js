5d86db521d029a825e91de7bd8cebcd341d08e10
matiaspepi <matiaspepi24@gmail.com>
2023-03-28 18:50:26 -0300

    primera entrega

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import NavBar from './componentes/NavBar/NavBar';
 import './App.css';
-
+import ItemListContainer from './componentes/ItemListContainer/ItemListContainer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <NavBar />
+    <ItemListContainer greeting={"La mejor teconologia en tus manos"} />
     </div>
   );
 }
---
+++ src/componentes/NavBar/NavBar.js
@@ -0,0 +1,53 @@
+import CartWidget from "../CartWidget/CartWidget"
+import "./NavBar.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import celular from "./assets/celulares.jpg"
+import tablets from "./assets/tablets.jpg"
+import notebooks from "./assets/notebooks.jpg"
+import Card from 'react-bootstrap/Card';
+const NavBar = () => {
+    return (
+        <nav>
+            <h3 className = "tittle">
+                Ecomerce
+            </h3>
+        <div className ="content-main">
+          <Card className= "cards" style={{ width: '18rem',height: '400px' }}>
+                <Card.Img variant="top" src= {celular} style= {{height: '195px'}}/>
+                   <Card.Body>
+                  <Card.Title>Celulares</Card.Title>
+                  <Card.Text>
+                     Encontrá lo que buscás en Celulares. Todo lo que necesitás lo conseguís en un solo lugar.
+                  </Card.Text>
+                  <Button variant="primary">Comprar</Button>
+                </Card.Body>
+              </Card>
+
+              <Card className= "cards"  style={{ width: '18rem', height: '400px'}}>
+                <Card.Img variant="top" src= {tablets}  />
+                   <Card.Body>
+                  <Card.Title>Tablets</Card.Title>
+                  <Card.Text>
+                        Encontrá Tablets de todo tipo y aprovechá las Ofertas en marcas de primer nivel.
+                  </Card.Text>
+                  <Button variant="primary">Comprar</Button>
+                </Card.Body>
+              </Card>
+
+              <Card className= "cards" style={{ width: '18rem'}}>
+                <Card.Img variant="top" src= {notebooks}/>
+                   <Card.Body>
+                  <Card.Title>Notebooks</Card.Title>
+                  <Card.Text>
+                      Descubrí los productos más buscados que no te podés perder en Notebooks ✓ Con Envío Gratis y Rápido
+                  </Card.Text>
+                  <Button variant="primary">Comprar</Button>
+                </Card.Body>
+              </Card>
+              </div>  
+        <CartWidget />
+        </nav>
+    )
+}
+export default NavBar
